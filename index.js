7e2aa57bcc8c21d7fbb0e6d7decf751c4692ed6d
Catherine Hu <catherinehu_9898@hotmail.com>
2021-07-02 00:01:29 +0000

    added fetchCoordsByIP

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -8,3 +8,12 @@ fetchMyIP((error, ip) => {
 
   console.log('It worked! Returned IP:', ip);
 });
+
+/* fetchCoordsByIP('162.245.144.188', (error, coords) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+  console.log('It worked! Returned coordinates: ', coords);
+});
+ */
--- iss.js
+++ iss.js
@@ -14,5 +14,18 @@ const fetchMyIP = (callback) => {
   });
 };
 
+const fetchCoordsByIP = (ip, callback) => {
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+    if (error) return callback(error, null);
 
-module.exports = { fetchMyIP };
+    if (response.statusCode !== 200) {
+      callback(Error(`Status code ${response.statusCode} when fetching IP : ${body}`), null);
+      return;
+    }
+
+    const { latitude, longitude } = JSON.parse(body);
+    callback(null, { latitude, longitude });
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
